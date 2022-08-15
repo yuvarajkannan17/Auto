b30e33d57e55ba78ed6d13a9ba0598f0360cca51
yuvarajkannan17 <yuvarajkannan17@gmail.com>
2022-08-15 05:59:37 +0530

    vechile

---
+++ src/test/java/com/transport/Automobile.java
@@ -0,0 +1,15 @@
+package com.transport;
+
+public class Automobile {
+	
+	private void bike() {
+System.out.println("RE");
+	}
+	private void car() {
+		System.out.println("RollsRoyce");
+			}
+	private void airLines() {
+		System.out.println("AirIndia");
+			}
+
+}
